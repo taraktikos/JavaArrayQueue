bfffc3ba48c585ebfa6830c855ce86eb8763081b
tsuhovenko <tsuhovenko@cogniance.com>
2014-10-28 15:43:59 +0200

    fix

--- src/main/java/ArrayQueue.java
+++ src/main/java/ArrayQueue.java
@@ -1,60 +1,57 @@
-import java.util.Arrays;
-
 /**
  * Created by Taras S on 27.10.2014.
  */
 public class ArrayQueue {
 
-    private int length = 5;
-    private int[] array;
-    private int first, next;
+    static final int LENGTH = 5;
+    int[] array;
+    int first;
+    int next;
 
     public ArrayQueue() {
-        this.array = new int[this.length];
-        this.first = 2;
-        this.next = 2;
+        this(ArrayQueue.LENGTH);
+    }
+
+    public ArrayQueue(int length) {
+        array = new int[length];
     }
 
     public ArrayQueue add(int item) {
-        this.array[this.next] = item;
-        this.next = (this.next + 1) % this.array.length;
-        if (this.full()) {
-            this.resize();
+        array[next] = item;
+        next = (next + 1) % array.length;
+        if (next % array.length == first) {
+            resize();
         }
         return this;
     }
 
     public int remove() {
-        if (this.empty()) {
+        if (isEmpty()) {
             return 0;
         }
-        int result = this.array[this.first];
-        this.array[this.first] = 0;
-        this.first = (this.first + 1) % this.array.length;
+        int result = array[first];
+        array[first] = 0;
+        first = (first + 1) % array.length;
         return result;
     }
 
-    public boolean empty() {
-        return this.first == this.next;
+    boolean isEmpty() {
+        return first == next;
     }
 
-    private boolean full() {
-        return (this.next) % this.array.length == this.first;
+    void resize() {
+        int[] newArray = new int[array.length * 2];
+
+        System.arraycopy(array, first, newArray, 0, array.length - first);
+        System.arraycopy(array, 0, newArray, array.length - first, next);
+
+        first = 0;
+        next = array.length;
+        array = newArray;
     }
 
-    private void resize() {
-        int[] newArray = new int[this.array.length * 2];
-        int count = this.first;
-
-        for (int i=this.first; i<this.array.length; i++) {
-            newArray[count++] = this.array[i];
-        }
-        for (int i=0; i<this.next; i++) {
-            newArray[count++] = this.array[i];
-        }
-
-        this.next = count;
-        this.array = newArray;
+    int size() {
+        return array.length;
     }
 
 }
--- src/test/java/ArrayQueueTest.java
+++ src/test/java/ArrayQueueTest.java
@@ -1,27 +1,67 @@
 import org.junit.Test;
 
-import java.lang.reflect.Field;
-
 import static org.junit.Assert.*;
 
 public class ArrayQueueTest {
 
     @Test
-    public void testAdd() throws Exception {
+    public void testAdd() {
         ArrayQueue queue = new ArrayQueue();
 
         queue.add(1).add(2).add(3).add(4);
-        System.out.println("Remove " + queue.remove());
-        System.out.println("Remove " + queue.remove());
-        System.out.println("Remove " + queue.remove());
-        System.out.println("Remove " + queue.remove());
-        System.out.println("Remove " + queue.remove());
-        System.out.println("Remove " + queue.remove());
-        System.out.println("Remove " + queue.remove());
-        System.out.println("Remove " + queue.remove());
-        queue.add(5);
-        queue.add(6);
+        assertEquals(1, queue.remove());
+        assertEquals(2, queue.remove());
+        assertEquals(3, queue.remove());
+        assertEquals(4, queue.remove());
+
+        queue.add(1).add(2).add(3).add(4);
+        assertEquals(1, queue.remove());
+        assertEquals(2, queue.remove());
+        queue.add(5).add(6);
+        assertEquals(5, queue.size());
+
         queue.add(7);
-        System.out.println("Remove " + queue.remove());
+        assertEquals(10, queue.size());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        ArrayQueue queue = new ArrayQueue();
+
+        assertTrue(queue.isEmpty());
+        queue.add(1).add(2).add(3).add(4);
+        assertFalse(queue.isEmpty());
+        queue.add(5);
+        assertFalse(queue.isEmpty());
+        queue.add(5);
+        assertFalse(queue.isEmpty());
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    public void testRemoveFromEmpty() {
+        ArrayQueue queue = new ArrayQueue();
+        assertEquals(0, queue.remove());
+    }
+
+    @Test
+    public void testRemove() {
+        ArrayQueue queue = new ArrayQueue();
+
+        queue.add(1).add(2);
+        assertEquals(1, queue.remove());
+
+        queue.add(3).add(4);
+        assertEquals(2, queue.remove());
+        assertEquals(3, queue.remove());
+        queue.add(5).add(6).add(7).add(8).add(9);
+
+        assertEquals(4, queue.remove());
     }
 }
