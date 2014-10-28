9c7c0977089fe9a3d3a0028e4c6f39dfa594df82
tsuhovenko <tsuhovenko@cogniance.com>
2014-10-28 12:09:56 +0200

    fix

--- src/main/java/ArrayQueue.java
+++ src/main/java/ArrayQueue.java
@@ -16,22 +16,21 @@ public class ArrayQueue {
     }
 
     public ArrayQueue add(int item) {
-        System.out.println("Is full " + this.full());
+        this.array[this.next] = item;
+        this.next = (this.next + 1) % this.array.length;
         if (this.full()) {
             this.resize();
         }
-        System.out.println("Add " + item);
-        this.array[this.next] = item;
-        System.out.println(this.next + " element is " + this.array[this.next]);
-        System.out.println("New array " + Arrays.toString(this.array));
-        this.next = (this.next + 1) % this.array.length;
-        System.out.println("Next index " + this.next);
         return this;
     }
 
     public int remove() {
+        if (this.empty()) {
+            return 0;
+        }
         int result = this.array[this.first];
-        this.first++;
+        this.array[this.first] = 0;
+        this.first = (this.first + 1) % this.array.length;
         return result;
     }
 
@@ -40,14 +39,21 @@ public class ArrayQueue {
     }
 
     private boolean full() {
-        return (this.next + 1) % this.array.length == this.first;
+        return (this.next) % this.array.length == this.first;
     }
 
     private void resize() {
         int[] newArray = new int[this.array.length * 2];
-        for (int i=0; i<this.array.length; i++) {
-            newArray[i] = this.array[i];
+        int count = this.first;
+
+        for (int i=this.first; i<this.array.length; i++) {
+            newArray[count++] = this.array[i];
         }
+        for (int i=0; i<this.next; i++) {
+            newArray[count++] = this.array[i];
+        }
+
+        this.next = count;
         this.array = newArray;
     }
 
--- src/test/java/ArrayQueueTest.java
+++ src/test/java/ArrayQueueTest.java
@@ -7,6 +7,18 @@ public class ArrayQueueTest {
     @Test
     public void testAdd() throws Exception {
         ArrayQueue queue = new ArrayQueue();
-        queue.add(1).add(2).add(3).add(4).add(5).add(6);
+        queue.add(1).add(2).add(3).add(4);
+        System.out.println("Remove " + queue.remove());
+        System.out.println("Remove " + queue.remove());
+        System.out.println("Remove " + queue.remove());
+        System.out.println("Remove " + queue.remove());
+        System.out.println("Remove " + queue.remove());
+        System.out.println("Remove " + queue.remove());
+        System.out.println("Remove " + queue.remove());
+        System.out.println("Remove " + queue.remove());
+        queue.add(5);
+        queue.add(6);
+        queue.add(7);
+        System.out.println("Remove " + queue.remove());
     }
 }
