20f34466f716d8a04b64e994062f5225490c0e64
tsuhovenko <tsuhovenko@cogniance.com>
2014-11-04 12:31:29 +0200

    test

--- src/main/java/my/parse/FileCalc.java
+++ src/main/java/my/parse/FileCalc.java
@@ -10,8 +10,20 @@ public class FileCalc {
         int c;
 
         while ((c = in.read()) != -1) {
-            System.out.print((char)c);
+            //System.out.print((char)c);
         }
+
+        InputStream is = new ByteArrayInputStream("a\nb\nc\n".getBytes());
+
+        // read it with BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        br.close();
     }
 
 }
