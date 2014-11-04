191745844028de072fff754ec622383543797071
tsuhovenko <tsuhovenko@cogniance.com>
2014-11-04 17:26:49 +0200

    test

---
+++ src/main/java/my/parse/CalcProcessor.java
@@ -0,0 +1,49 @@
+package my.parse;
+
+import my.math.Evaluable;
+
+import java.io.*;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CalcProcessor {
+
+    public static void process(InputStream input, OutputStream output)  throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
+        String line;
+
+        HashMap<Integer, Evaluable> functions = new HashMap<>();
+        int key = 0;
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                functions.put(key++, new Calc(line));
+            }
+        } catch (ParseException e) {
+            System.out.println("Parse exception " + e.getMessage());
+        }
+        bufferedReader.close();
+
+        HashMap<String, Integer> map = new HashMap<>();
+        String header = "x\t\t\t";
+        for (Map.Entry<Integer, Evaluable> function: functions.entrySet()) {
+            header += "f(" + function.getKey() + ")\t\t\t";
+        }
+        output.write((header + "\n").getBytes());
+        for (int i = 0; i <= 10; i++) {
+            map.put("x", i);
+            StringBuilder resultRow = new StringBuilder();
+            resultRow.append(i).append("\t\t\t");
+            for (Map.Entry<Integer, Evaluable> function: functions.entrySet()) {
+                try {
+                    resultRow.append(function.getValue().evaluate(map)).append("\t\t\t");
+                } catch (RuntimeException e) {
+                    resultRow.append(e.getMessage()).append("\t\t\t");
+                }
+            }
+            resultRow.append("\n");
+            output.write(resultRow.toString().getBytes());
+        }
+        output.close();
+    }
+}
--- src/main/java/my/parse/FileCalc.java
+++ src/main/java/my/parse/FileCalc.java
@@ -10,58 +10,9 @@ import java.util.Map.Entry;
 public class FileCalc {
 
     public static void main(String[] args) throws IOException {
-
-//        StringReader in = new StringReader(BufferedInputFile.read("/home/taras/FileCalc.java"));
-//        int c;
-//
-//        while ((c = in.read()) != -1) {
-//            //System.out.print((char)c);
-//        }
-
-        InputStream is = new ByteArrayInputStream("100000*x*x*x*x*x*x/(x-1)\nx\n2+2\n".getBytes());
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-
-        String line;
-
-        HashMap<Integer, Evaluable> functions = new HashMap<>();
-        int key = 0;
-        try {
-            while ((line = br.readLine()) != null) {
-                functions.put(key++, new Calc(line));
-            }
-        } catch (ParseException e) {
-            System.out.println("Parse exception " + e.getMessage());
-        }
-
-        HashMap<String, Integer> map = new HashMap<>();
-        String header = "x\t\t\t";
-        for (Entry<Integer, Evaluable> function: functions.entrySet()) {
-            header += "f(" + function.getKey() + ")\t\t\t";
-        }
-        String result = header + "\n";
-        for (int i = 0; i <= 10; i++) {
-            if (map.get("x") == null) {
-                map.put("x", i);
-            } else {
-                map.replace("x", i);
-            }
-            result += i + "\t\t\t";
-
-            for (Entry<Integer, Evaluable> function: functions.entrySet()) {
-                try {
-                    result += function.getValue().evaluate(map) + "\t\t\t";
-                } catch (RuntimeException e) {
-                    result += e.getMessage() + "\t\t\t";
-                }
-            }
-            result += "\n";
-        }
-        br.close();
-
-        String file = "/home/taras/test.out";
-        PrintWriter out = new PrintWriter(file);
-        out.print(result);
-        out.close();
+        InputStream input = new ByteArrayInputStream("100000*x*x*x*x*x*x/(x-1)\nx\n2+2\n".getBytes());
+        FileOutputStream output = new FileOutputStream("/home/taras/test.out");
+        CalcProcessor.process(input, output);
 
         System.out.println("Done");
     }
